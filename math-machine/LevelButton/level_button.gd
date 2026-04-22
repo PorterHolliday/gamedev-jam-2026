6f183deee737c4c80dd63ee63829bee2c898bc2b
Proter Goater <porterholliday@gmail.com>
2026-04-21 21:15:29 -0600

    Moves enter level logic to level selection screen. Fixes tutorial animations.

--- math-machine/LevelButton/level_button.gd
+++ math-machine/LevelButton/level_button.gd
@@ -2,17 +2,14 @@
 class_name LevelButton
 extends Button
 
-signal level_completed
-
 @onready var level_number_label: Label = $LevelNumberLabel
 @onready var checkmark: TextureRect = %Checkmark
 
-@export var _level_scene: PackedScene
+@export var level_scene: PackedScene
 var level_number: int = 0:
 	set(new_val):
 		level_number = new_val
 		level_number_label.text = str(level_number)
-var level: Level
 
 var level_complete: bool = false:
 	set(new_val):
@@ -20,28 +17,8 @@ var level_complete: bool = false:
 		checkmark.visible = level_complete
 
 func _ready() -> void:
-	pressed.connect(_on_pressed)
 	mouse_entered.connect(_on_mouse_entered)
 	mouse_exited.connect(_on_mouse_exited)
-	
-func _on_pressed() -> void:
-	_enter_level()
-	
-func _enter_level() -> void:
-	level = _level_scene.instantiate()
-	level.restarted.connect(_on_level_restarted)
-	level.completed.connect(_on_level_completed)
-	get_parent().add_child(level)
-	
-func _on_level_restarted() -> void:
-	level.queue_free()
-	_enter_level()
-	
-func _on_level_completed() -> void:
-	level.completed.disconnect(_on_level_completed)
-	level.queue_free()
-	level_complete = true
-	level_completed.emit()
 	
 func _on_mouse_entered() -> void:
 	if not disabled:
--- math-machine/LevelSelectionScreen/level_selection_screen.gd
+++ math-machine/LevelSelectionScreen/level_selection_screen.gd
@@ -1,23 +1,51 @@
 extends Control
 
 @onready var back_button: Button = %BackButton
+var current_level_button: LevelButton
+var current_level: Level
 
 func _ready() -> void:
 	back_button.pressed.connect(_on_back_button_pressed)
-	
 	_number_level_buttons()
 	
 func _number_level_buttons() -> void:
-	var prev_child: LevelButton = null
 	var level_number: int = 1
 	for child in get_children():
 		if child is LevelButton:
-			if prev_child:
-				prev_child.level_completed.connect(func():
-					child.disabled = false)
-			prev_child = child
+			child.pressed.connect(func():
+				_on_level_button_pressed(child))
 			child.level_number = level_number
 			level_number += 1
+			
+func _on_level_button_pressed(button: LevelButton) -> void:
+	current_level_button = button
+	current_level = button.level_scene.instantiate()
+	current_level.restarted.connect(_on_level_restarted)
+	current_level.completed.connect(_on_level_completed)
+	add_child(current_level)
+	
+func _on_level_restarted() -> void:
+	current_level.queue_free()
+	current_level = current_level_button.level_scene.instantiate()
+	current_level.restarted.connect(_on_level_restarted)
+	current_level.completed.connect(_on_level_completed)
+	add_child(current_level)
+	
+func _on_level_completed() -> void:
+	current_level.completed.disconnect(_on_level_completed)
+	current_level.restarted.disconnect(_on_level_restarted)
+	current_level.queue_free()
+	current_level_button.level_complete = true
+	_enable_next_level_button()
+	
+func _enable_next_level_button() -> void:
+	var enable_next: bool = false
+	for child in get_children():
+		if enable_next and child is LevelButton:
+			child.disabled = false
+			break
+		if child == current_level_button:
+			enable_next = true
 
 func _on_back_button_pressed() -> void:
 	hide()
