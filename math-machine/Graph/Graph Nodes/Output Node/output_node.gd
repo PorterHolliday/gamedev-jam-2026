f2172c3af313d32bd92df6f084664e85e3b008eb
Proter Goater <porterholliday@gmail.com>
2026-07-08 07:51:32 -0600

    Implements output node fill animation.

--- math-machine/Graph/Graph Nodes/Output Node/output_node.gd
+++ math-machine/Graph/Graph Nodes/Output Node/output_node.gd
@@ -28,14 +28,18 @@ signal received_valid_output
 	set(new_val):
 		is_satisfied = new_val
 		if is_satisfied:
-			_sprite.modulate = satisfied_color
-			_value_label.modulate = satisfied_text_color
+			_play_fill_animation()
 		else:
-			_sprite.modulate = unsatisfied_color
-			_value_label.modulate = unsatisfied_text_color
+			_play_fill_animation(true)
+			
+@export var transition_duration: float = 0.5
 
 func _ready() -> void:
 	_update_value_label()
+	_sprite.material = _sprite.material.duplicate_deep(Resource.DEEP_DUPLICATE_ALL)
+	_value_label.material = _value_label.material.duplicate_deep(Resource.DEEP_DUPLICATE_ALL)
+	_sprite.material.set_shader_parameter('fill_color', satisfied_color)
+	_value_label.material.set_shader_parameter('fill_color', satisfied_text_color)
 	if Engine.is_editor_hint(): return
 	super()
 	
@@ -57,3 +61,20 @@ func _check_if_satisfied() -> void:
 func _update_value_label() -> void:
 	if not _value_label: return
 	_value_label.text = str(value)
+
+func _play_fill_animation(reverse: bool = false) -> void:
+	var sprite_material: ShaderMaterial = _sprite.material
+	#sprite_material.set_shader_parameter('invert', reverse)
+	var text_material: ShaderMaterial = _value_label.material
+	#text_material.set_shader_parameter('invert', reverse)
+	var tween: Tween = get_tree().create_tween()
+	tween.tween_method(func(value: float):
+		sprite_material.set_shader_parameter('progress', value), 
+		1.0 - float(!reverse), 1.0 - float(reverse), transition_duration
+	)
+	tween.parallel().tween_method(func(value: float):
+		text_material.set_shader_parameter('progress', value), 
+		1.0 - float(!reverse), 1.0 - float(reverse), transition_duration
+	)
+	
+	await tween.finished
